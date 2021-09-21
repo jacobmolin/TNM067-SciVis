5a7cf975ea4296e3a5f42ec9424bc3f6842dac5e
jacobmolin <jacob.molle.molin@gmail.com>
2021-09-21 17:25:14 +0200

    Lab 2 done

--- modules/tnm067lab1/processors/imageupsampler.cpp
+++ modules/tnm067lab1/processors/imageupsampler.cpp
@@ -8,123 +8,204 @@
 
 namespace inviwo {
 
-namespace detail {
+    namespace detail {
 
-template <typename T>
-void upsample(ImageUpsampler::IntepolationMethod method, const LayerRAMPrecision<T>& inputImage,
-              LayerRAMPrecision<T>& outputImage) {
-    using F = typename float_type<T>::type;
+        template <typename T>
+        void upsample(ImageUpsampler::IntepolationMethod method, const LayerRAMPrecision<T>& inputImage,
+                      LayerRAMPrecision<T>& outputImage) {
+            using F = typename float_type<T>::type;
 
-    const size2_t inputSize = inputImage.getDimensions();
-    const size2_t outputSize = outputImage.getDimensions();
+            const size2_t inputSize = inputImage.getDimensions();
+            const size2_t outputSize = outputImage.getDimensions();
 
-    const T* inPixels = inputImage.getDataTyped();
-    T* outPixels = outputImage.getDataTyped();
+            const T* inPixels = inputImage.getDataTyped();
+            T* outPixels = outputImage.getDataTyped();
 
-    auto inIndex = [&inputSize](auto pos) -> size_t {
-        pos = glm::clamp(pos, decltype(pos)(0), decltype(pos)(inputSize - size2_t(1)));
-        return pos.x + pos.y * inputSize.x;
-    };
-    auto outIndex = [&outputSize](auto pos) -> size_t {
-        pos = glm::clamp(pos, decltype(pos)(0), decltype(pos)(outputSize - size2_t(1)));
-        return pos.x + pos.y * outputSize.x;
-    };
+            auto inIndex = [&inputSize] (auto pos) -> size_t {
+                pos = glm::clamp(pos, decltype(pos)(0), decltype(pos)(inputSize - size2_t(1)));
+                return pos.x + pos.y * inputSize.x;
+            };
+            auto outIndex = [&outputSize] (auto pos) -> size_t {
+                pos = glm::clamp(pos, decltype(pos)(0), decltype(pos)(outputSize - size2_t(1)));
+                return pos.x + pos.y * outputSize.x;
+            };
 
-    util::forEachPixel(outputImage, [&](ivec2 outImageCoords) {
-        // outImageCoords: Exact pixel coordinates in the output image currently writing to
-        // inImageCoords: Relative coordinates of outImageCoords in the input image, might be
-        // between pixels
-        dvec2 inImageCoords =
-            ImageUpsampler::convertCoordinate(outImageCoords, inputSize, outputSize);
+            util::forEachPixel(outputImage, [&] (ivec2 outImageCoords) {
+                // outImageCoords: Exact pixel coordinates in the output image currently writing to
+                // inImageCoords: Relative coordinates of outImageCoords in the input image, might be
+                // between pixels
+                dvec2 inImageCoords =
+                    ImageUpsampler::convertCoordinate(outImageCoords, inputSize, outputSize);
 
-        T finalColor(0);
+                T finalColor(0);
 
-        // DUMMY COLOR, remove or overwrite this bellow
-        finalColor = inPixels[inIndex(
-            glm::clamp(size2_t(outImageCoords), size2_t(0), size2_t(inputSize - size2_t(1))))];
+                // DUMMY COLOR, remove or overwrite this bellow
+                finalColor = inPixels[inIndex(
+                    glm::clamp(size2_t(outImageCoords), size2_t(0), size2_t(inputSize - size2_t(1))))];
 
-        switch (method) {
-            case ImageUpsampler::IntepolationMethod::PiecewiseConstant: {
-                // Task 6
-                // Update finalColor
+                switch (method) {
+                case ImageUpsampler::IntepolationMethod::PiecewiseConstant: {
+                    // Task 6
+                    // Update finalColor
+                    // TODO: round(), ceil() or floor()???
+                    // inPixels contains the color of each pixel in inImage
+                    finalColor = inPixels[inIndex(round(inImageCoords))];
+                    break;
+                }
+                case ImageUpsampler::IntepolationMethod::Bilinear: {
+                    // Update finalColor
+
+                    // Get pixel
+
+                    // Get neighbouring pixels
+                    ivec2 pos0{ floor(inImageCoords) };
+                    ivec2 pos1{ ceil(inImageCoords.x), floor(inImageCoords.y) };
+                    ivec2 pos2{ floor(inImageCoords.x), ceil(inImageCoords.y) };
+                    ivec2 pos3{ ceil(inImageCoords) };
+
+                    // Get Get neighbouring pixels' values and store in v
+                    std::array<T, 4> v{
+                        inPixels[inIndex(pos0)],
+                        inPixels[inIndex(pos1)],
+                        inPixels[inIndex(pos2)],
+                        inPixels[inIndex(pos3)],
+                    };
+
+                    // Get parameterization in x- and y-direction
+                    double x_t = inImageCoords.x - pos0.x;
+                    double y_t = inImageCoords.y - pos0.y;
+
+                    finalColor = inviwo::TNM067::Interpolation::bilinear(v, x_t, y_t);
+                    break;
+                }
+                case ImageUpsampler::IntepolationMethod::Biquadratic: {
+                    // Update finalColor
+
+                    // Get neighbouring pixels
+                    ivec2 pos0{ floor(inImageCoords) };
+                    ivec2 pos1{ ceil(inImageCoords.x), floor(inImageCoords.y) };
+                    ivec2 pos2{ ceil(inImageCoords.x) + 1, floor(inImageCoords.y) };
+                    ivec2 pos3{ floor(inImageCoords.x), ceil(inImageCoords.y) };
+                    ivec2 pos4{ ceil(inImageCoords) };
+                    ivec2 pos5{ ceil(inImageCoords.x) + 1, ceil(inImageCoords.y) };
+                    ivec2 pos6{ floor(inImageCoords.x), ceil(inImageCoords.y) + 1 };
+                    ivec2 pos7{ ceil(inImageCoords.x), ceil(inImageCoords.y) + 1 };
+                    ivec2 pos8{ ceil(inImageCoords.x) + 1, ceil(inImageCoords.y) + 1 };
+
+                    // Get Get neighbouring pixels' values and store in v
+                    std::array<T, 9> v{
+                        inPixels[inIndex(pos0)],
+                        inPixels[inIndex(pos1)],
+                        inPixels[inIndex(pos2)],
+                        inPixels[inIndex(pos3)],
+                        inPixels[inIndex(pos4)],
+                        inPixels[inIndex(pos5)],
+                        inPixels[inIndex(pos6)],
+                        inPixels[inIndex(pos7)],
+                        inPixels[inIndex(pos8)],
+                    };
+
+                    // Get parameterization in x- and y-direction
+                    double x_t = inImageCoords.x - pos0.x;
+                    double y_t = inImageCoords.y - pos0.y;
+
+                    finalColor = inviwo::TNM067::Interpolation::biQuadratic(v, x_t / 2.0, y_t / 2.0);
+                    break;
+                }
+                case ImageUpsampler::IntepolationMethod::Barycentric: {
+                    // Update finalColor
+
+                    // Get neighbouring pixels
+                    ivec2 pos0{ floor(inImageCoords) };
+                    ivec2 pos1{ ceil(inImageCoords.x), floor(inImageCoords.y) };
+                    ivec2 pos2{ floor(inImageCoords.x), ceil(inImageCoords.y) };
+                    ivec2 pos3{ ceil(inImageCoords) };
+
+                    // Get Get neighbouring pixels' values and store in v
+                    std::array<T, 4> v{
+                        inPixels[inIndex(pos0)],
+                        inPixels[inIndex(pos1)],
+                        inPixels[inIndex(pos2)],
+                        inPixels[inIndex(pos3)],
+                    };
+
+                    // Get parameterization in x- and y-direction
+                    double x_t = inImageCoords.x - pos0.x;
+                    double y_t = inImageCoords.y - pos0.y;
+
+                    finalColor = inviwo::TNM067::Interpolation::barycentric(v, x_t, y_t);
+
+
+                    break;
+                }
+                default:
                 break;
-            }
-            case ImageUpsampler::IntepolationMethod::Bilinear: {
-                // Update finalColor
-                break;
-            }
-            case ImageUpsampler::IntepolationMethod::Biquadratic: {
-                // Update finalColor
-                break;
-            }
-            case ImageUpsampler::IntepolationMethod::Barycentric: {
-                // Update finalColor
-                break;
-            }
-            default:
-                break;
+                }
+
+                outPixels[outIndex(outImageCoords)] = finalColor;
+                               });
         }
 
-        outPixels[outIndex(outImageCoords)] = finalColor;
-    });
-}
+    }  // namespace detail
 
-}  // namespace detail
+    const ProcessorInfo ImageUpsampler::processorInfo_{
+        "org.inviwo.imageupsampler",  // Class identifier
+        "Image Upsampler",            // Display name
+        "TNM067",                     // Category
+        CodeState::Experimental,      // Code state
+        Tags::None,                   // Tags
+    };
+    const ProcessorInfo ImageUpsampler::getProcessorInfo() const { return processorInfo_; }
 
-const ProcessorInfo ImageUpsampler::processorInfo_{
-    "org.inviwo.imageupsampler",  // Class identifier
-    "Image Upsampler",            // Display name
-    "TNM067",                     // Category
-    CodeState::Experimental,      // Code state
-    Tags::None,                   // Tags
-};
-const ProcessorInfo ImageUpsampler::getProcessorInfo() const { return processorInfo_; }
-
-ImageUpsampler::ImageUpsampler()
-    : Processor()
-    , inport_("inport", true)
-    , outport_("outport", true)
-    , interpolationMethod_("interpolationMethod", "Interpolation Method",
-                           {
-                               {"piecewiseconstant", "Piecewise Constant (Nearest Neighbor)",
-                                IntepolationMethod::PiecewiseConstant},
-                               {"bilinear", "Bilinear", IntepolationMethod::Bilinear},
-                               {"biquadratic", "Biquadratic", IntepolationMethod::Biquadratic},
-                               {"barycentric", "Barycentric", IntepolationMethod::Barycentric},
-                           }) {
-    addPort(inport_);
-    addPort(outport_);
-    addProperty(interpolationMethod_);
-}
-
-void ImageUpsampler::process() {
-    auto inputImage = inport_.getData();
-    if (inputImage->getDataFormat()->getComponents() != 1) {
-        LogError("The ImageUpsampler processor does only support single channel images");
+    ImageUpsampler::ImageUpsampler()
+        : Processor()
+        , inport_("inport", true)
+        , outport_("outport", true)
+        , interpolationMethod_("interpolationMethod", "Interpolation Method",
+                               {
+                                   { "piecewiseconstant", "Piecewise Constant (Nearest Neighbor)",
+                                   IntepolationMethod::PiecewiseConstant },
+                               { "bilinear", "Bilinear", IntepolationMethod::Bilinear },
+                               { "biquadratic", "Biquadratic", IntepolationMethod::Biquadratic },
+                               { "barycentric", "Barycentric", IntepolationMethod::Barycentric },
+                               }) {
+        addPort(inport_);
+        addPort(outport_);
+        addProperty(interpolationMethod_);
     }
 
-    auto inSize = inport_.getData()->getDimensions();
-    auto outDim = outport_.getDimensions();
+    void ImageUpsampler::process() {
+        auto inputImage = inport_.getData();
+        if (inputImage->getDataFormat()->getComponents() != 1) {
+            LogError("The ImageUpsampler processor does only support single channel images");
+        }
 
-    auto outputImage = std::make_shared<Image>(outDim, inputImage->getDataFormat());
-    outputImage->getColorLayer()->setSwizzleMask(inputImage->getColorLayer()->getSwizzleMask());
-    outputImage->getColorLayer()
-        ->getEditableRepresentation<LayerRAM>()
-        ->dispatch<void, dispatching::filter::Scalars>([&](auto outRep) {
+        auto inSize = inport_.getData()->getDimensions();
+        auto outDim = outport_.getDimensions();
+
+        auto outputImage = std::make_shared<Image>(outDim, inputImage->getDataFormat());
+        outputImage->getColorLayer()->setSwizzleMask(inputImage->getColorLayer()->getSwizzleMask());
+        outputImage->getColorLayer()
+            ->getEditableRepresentation<LayerRAM>()
+            ->dispatch<void, dispatching::filter::Scalars>([&] (auto outRep) {
             auto inRep = inputImage->getColorLayer()->getRepresentation<LayerRAM>();
             detail::upsample(interpolationMethod_.get(), *(const decltype(outRep))(inRep), *outRep);
-        });
+                                                           });
 
-    outport_.setData(outputImage);
-}
+        outport_.setData(outputImage);
+    }
 
-dvec2 ImageUpsampler::convertCoordinate(ivec2 outImageCoords, size2_t inputSize, size2_t outputSize) {
-    // TODO implement
-    dvec2 c(outImageCoords);
+    dvec2 ImageUpsampler::convertCoordinate(ivec2 outImageCoords, size2_t inputSize, size2_t outputSize) {
+        // TODO implement
+        // copy of outPutImageCoords
+        dvec2 c(outImageCoords);
 
-    // TASK 5: Convert the outImageCoords to its coordinates in the input image
+        // TASK 5: Convert the outImageCoords to its coordinates in the input image
+        // Get the scaling factor between inputSize and outputSize
+        dvec2 factor = dvec2(inputSize) / dvec2(outputSize);
+        c = c * factor;
 
-    return c;
-}
+        return c;
+    }
 
 }  // namespace inviwo
--- modules/tnm067lab1/utils/interpolationmethods.h
+++ modules/tnm067lab1/utils/interpolationmethods.h
@@ -6,106 +6,142 @@
 
 namespace inviwo {
 
-template <typename T>
-struct float_type {
-    using type = double;
-};
+    template <typename T>
+    struct float_type {
+        using type = double;
+    };
 
-template <>
-struct float_type<float> {
-    using type = float;
-};
-template <>
-struct float_type<vec3> {
-    using type = float;
-};
-template <>
-struct float_type<vec2> {
-    using type = float;
-};
-template <>
-struct float_type<vec4> {
-    using type = float;
-};
+    template <>
+    struct float_type<float> {
+        using type = float;
+    };
+    template <>
+    struct float_type<vec3> {
+        using type = float;
+    };
+    template <>
+    struct float_type<vec2> {
+        using type = float;
+    };
+    template <>
+    struct float_type<vec4> {
+        using type = float;
+    };
 
-namespace TNM067 {
-namespace Interpolation {
+    namespace TNM067 {
+        namespace Interpolation {
 
 #define ENABLE_LINEAR_UNITTEST 0
-template <typename T, typename F = double>
-T linear(const T& a, const T& b, F x) {
-    if (x <= 0) return a;
-    if (x >= 1) return b;
+            template <typename T, typename F = double>
+            T linear(const T& a, const T& b, F x) {
+                if (x <= 0) return a;
+                if (x >= 1) return b;
 
-    return  a;
-}
+                // In Lec3, p10
+                // a = x1
+                // b = x2
+                // x = t
 
-// clang-format off
-    /*
-     2------3
-     |      |
-    y|  •   |
-     |      |
-     0------1
-        x
-    */
-    // clang format on
+                T f = a * (1.0 - x) + b * x;
+                return  f;
+            }
+
+            // clang-format off
+                /*
+                 2------3
+                 |      |
+                y|  ï¿½   |
+                 |      |
+                 0------1
+                    x
+                */
+                // clang format on
 #define ENABLE_BILINEAR_UNITTEST 0
-template<typename T, typename F = double> 
-T bilinear(const std::array<T, 4> &v, F x, F y) {
-    return v[0];
-}
+            template<typename T, typename F = double>
+            T bilinear(const std::array<T, 4>& v, F x, F y) {
+
+                T f_x_y1 = linear(v[0], v[1], x);
+                T f_x_y2 = linear(v[2], v[3], x);
+                T f = linear(f_x_y1, f_x_y2, y);
+
+                return f;
+            }
 
 
-    // clang-format off
-    /* 
-    a--•----b------c
-    0  x    1      2
-    */
-// clang-format on
+            // clang-format off
+            /*
+            a--ï¿½----b------c
+            0  x    1      2
+            */
+            // clang-format on
 #define ENABLE_QUADRATIC_UNITTEST 0
-template <typename T, typename F = double>
-T quadratic(const T& a, const T& b, const T& c, F x) {
-    return a;
-}
+            template <typename T, typename F = double>
+            T quadratic(const T& a, const T& b, const T& c, F x) {
+                T f = (1 - x) * (1 - 2 * x) * a + 4 * x * (1 - x) * b + x * (2 * x - 1) * c;
+                return f;
+            }
 
-// clang-format off
-    /* 
-    6-------7-------8
-    |       |       |
-    |       |       |
-    |       |       |
-    3-------4-------5
-    |       |       |
-   y|  •    |       |
-    |       |       |
-    0-------1-------2
-    0  x    1       2
-    */
-// clang-format on
+            // clang-format off
+                /*
+                6-------7-------8
+                |       |       |
+                |       |       |
+                |       |       |
+                3-------4-------5
+                |       |       |
+               y|  ï¿½    |       |
+                |       |       |
+                0-------1-------2
+                0  x    1       2
+                */
+                // clang-format on
 #define ENABLE_BIQUADRATIC_UNITTEST 0
-template <typename T, typename F = double>
-T biQuadratic(const std::array<T, 9>& v, F x, F y) {
-    return v[0];
-}
+            template <typename T, typename F = double>
+            T biQuadratic(const std::array<T, 9>& v, F x, F y) {
+                T f1 = quadratic(v[0], v[1], v[2], x);
+                T f2 = quadratic(v[3], v[4], v[5], x);
+                T f3 = quadratic(v[6], v[7], v[8], x);
 
-// clang-format off
-    /*
-     2---------3
-     |'-.      |
-     |   -,    |
-   y |  •  -,  |
-     |       -,|
-     0---------1
-        x
-    */
-// clang-format on
+                T f = quadratic(f1, f2, f3, y);
+                return f;
+            }
+
+            // clang-format off
+                /*
+                 2---------3
+                 |'-.      |
+                 |   -,    |
+               y |  ï¿½  -,  |
+                 |       -,|
+                 0---------1
+                    x
+                */
+                // clang-format on
 #define ENABLE_BARYCENTRIC_UNITTEST 0
-template <typename T, typename F = double>
-T barycentric(const std::array<T, 4>& v, F x, F y) {
-    return v[0];
-}
+            template <typename T, typename F = double>
+            T barycentric(const std::array<T, 4>& v, F x, F y) {
+                float alpha;
+                float beta;
+                float gamma;
+                T f_a;
 
-}  // namespace Interpolation
-}  // namespace TNM067
+                if (x + y < 1.0) {
+                    alpha = 1.0 - (x + y);
+                    beta = x;
+                    gamma = y;
+                    f_a = v[0];
+                }
+                else {
+                    alpha = (x + y) - 1.0;
+                    beta = 1.0 - y;
+                    gamma = 1.0 - x;
+                    f_a = v[3];
+                }
+
+                T f = alpha * f_a + beta * v[1] + gamma * v[2];
+                return f;
+            }
+
+        }  // namespace Interpolation
+    }  // namespace TNM067
 }  // namespace inviwo
